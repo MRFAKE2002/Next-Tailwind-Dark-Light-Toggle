c0be9f02177d06499f769d0026cfa7bd9e164bba
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-05 18:36:00 +0330

    Make DarkToggleButton.tsx component

---
+++ src/components/buttons/DarkToggleButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+//! Libraries
+import React, { useEffect, useState } from "react";
+
+//! Type
+type Props = {};
+
+function DarkToggleButton({}: Props) {
+  //! useState
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  //! Custom Function
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  //! useEffect
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  //! JSX
+
+  return (
+    <button
+      className="fixed top-3 lg:top-4 right-3 lg:right-4 w-9 h-9 lg:w-10 lg:h-10 rounded-full flex justify-center items-center shadow-lg bg-amber-500 text-neutral-950 hover:bg-amber-600 transition-colors"
+      onClick={toggleDarkMode}
+    >
+      <i className={`bx bx-${isDarkMode ? "sun" : "moon"} text-lg lg:text-xl`}></i>
+    </button>
+  );
+}
+
+export default DarkToggleButton;
