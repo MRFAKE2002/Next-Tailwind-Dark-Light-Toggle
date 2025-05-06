66674cdb7eb7f3341b846ce6f264c4a617f4d0a5
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-06 21:16:47 +0330

    Add Local Stage for dark mode

--- src/components/buttons/DarkToggleButton.tsx
+++ src/components/buttons/DarkToggleButton.tsx
@@ -9,7 +9,7 @@ type Props = {};
 function DarkToggleButton({}: Props) {
   //! useState
 
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   //! Custom Function
 
@@ -20,11 +20,24 @@ function DarkToggleButton({}: Props) {
   //! useEffect
 
   useEffect(() => {
+    const savedTheme = localStorage.getItem("darkMode");
+    if (savedTheme) {
+      setIsDarkMode(JSON.parse(savedTheme));
+    } else {
+      const webDefaultTheme = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setIsDarkMode(webDefaultTheme);
+    }
+  }, []);
+
+  useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
+    localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
   }, [isDarkMode]);
 
   //! JSX
@@ -34,7 +47,9 @@ function DarkToggleButton({}: Props) {
       className="fixed top-3 lg:top-4 right-3 lg:right-4 w-9 h-9 lg:w-10 lg:h-10 rounded-full flex justify-center items-center shadow-lg bg-amber-500 text-neutral-950 hover:bg-amber-600 transition-colors"
       onClick={toggleDarkMode}
     >
-      <i className={`bx bx-${isDarkMode ? "sun" : "moon"} text-lg lg:text-xl`}></i>
+      <i
+        className={`bx bx-${isDarkMode ? "sun" : "moon"} text-lg lg:text-xl`}
+      ></i>
     </button>
   );
 }
