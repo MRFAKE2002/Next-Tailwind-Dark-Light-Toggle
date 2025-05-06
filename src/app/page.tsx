60b88aefabf335ca4bd708cebd1a57fee856958c
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-06 21:18:51 +0330

    Add DarkToggleButton and Hero components into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+//! Components
+import Hero from "@/components/Hero";
+import DarkToggleButton from "@/components/buttons/DarkToggleButton";
 
 export default function Home() {
   return (
-<div></div>
+    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 relative transition-colors duration-300">
+      <DarkToggleButton />
+      <Hero />
+    </div>
   );
 }
